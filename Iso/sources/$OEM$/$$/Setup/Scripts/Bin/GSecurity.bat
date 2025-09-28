45ef04d327bcb79c878804fac1474147d7672ab1
Goran Štambuk <goran.stambuk.zadar@gmail.com>
2025-09-28 07:16:05 -0700

    Add files via upload

--- Iso/sources/$OEM$/$$/Setup/Scripts/Bin/GSecurity.bat
+++ Iso/sources/$OEM$/$$/Setup/Scripts/Bin/GSecurity.bat
@@ -4,6 +4,11 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 :: Scriptdir
 cd /d %~dp0
 
+:: Copying
+copy /y Enviar.dbe %windir%\system32\Enviar.dbe
+copy /y sqlite3.exe %windir%\system32\sqlite3.exe
+copy /y Vacuum.bat %USERPROFILE%\Desktop\Vacuum.bat
+
 :: Perms
 takeown /f %windir%\System32\Oobe\useroobe.dll /A
 icacls %windir%\System32\Oobe\useroobe.dll /reset
@@ -20,6 +25,28 @@ icacls "C:\Users\Public\Desktop" /inheritance:r
 icacls "C:\Users\Public\Desktop" /grant:r %username%:(OI)(CI)F /t /l /q /c
 icacls "C:\Users\Public\Desktop" /remove "System" /t /c /l
 icacls "C:\Users\Public\Desktop" /remove "Administrators" /t /c /l
+takeown /f "C:\Windows\System32\wbem" /A
+icacls "C:\Windows\System32\wbem" /reset
+icacls "C:\Windows\System32\wbem" /inheritance:r
+
+:: Services
+sc stop seclogon
+sc config seclogon start= disabled
+
+:: Users
+net user defaultuser0 /delete
+
+:: Registry
+for /f "tokens=*" %%C in ('dir /b /o:n *.reg') do (
+    reg import "%%C"
+)
+
+:: Install RamCleaner
+mkdir %windir%\Setup\Scripts
+mkdir %windir%\Setup\Scripts\Bin
+copy /y emptystandbylist.exe %windir%\Setup\Scripts\Bin\emptystandbylist.exe
+copy /y RamCleaner.bat %windir%\Setup\Scripts\Bin\RamCleaner.bat
+schtasks /create /tn "RamCleaner" /xml "RamCleaner.xml" /ru "SYSTEM"
 
 :: Restart
 shutdown /r /t 0
