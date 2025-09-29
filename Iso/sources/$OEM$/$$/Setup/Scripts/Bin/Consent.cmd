749907092118e0b026dbbe02fd17358cf19586df
Goran Štambuk <goran.stambuk.zadar@gmail.com>
2025-09-29 06:52:01 +0200

    Delete Iso/sources/$OEM$/$$/Setup/Scripts/Bin directory

--- Iso/sources/$OEM$/$$/Setup/Scripts/Bin/Consent.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-takeown /f %windir%\system32\consent.exe /A
-icacls %windir%\system32\consent.exe /reset
-icacls %windir%\system32\consent.exe /inheritance:r
-icacls %windir%\system32\consent.exe /grant:r "Console Logon":RX
-Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "1" /f
-Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorUser" /t REG_DWORD /d "1" /f
-Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "1" /f
-Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorUser" /t REG_DWORD /d "1" /f
