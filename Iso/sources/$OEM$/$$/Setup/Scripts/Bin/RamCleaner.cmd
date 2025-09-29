fa45bee33660f4418ac21f3b569fd56e2d8fe9f1
Goran Štambuk <goran.stambuk.zadar@gmail.com>
2025-09-29 09:17:32 +0200

    Add files via upload

---
+++ Iso/sources/$OEM$/$$/Setup/Scripts/Bin/RamCleaner.cmd
@@ -0,0 +1,6 @@
+:: Install RamCleaner
+mkdir %windir%\Setup\Scripts
+mkdir %windir%\Setup\Scripts\Bin
+copy /y emptystandbylist.exe %windir%\Setup\Scripts\Bin\emptystandbylist.exe
+copy /y RamCleaner.bat %windir%\Setup\Scripts\Bin\RamCleaner.bat
+schtasks /create /tn "RamCleaner" /xml "RamCleaner.xml" /ru "SYSTEM"
