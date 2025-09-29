c5424320ccca0a0e601adc21cdcda29f9acaf9a0
Goran Štambuk <goran.stambuk.zadar@gmail.com>
2025-09-29 06:55:02 +0100

    Add files via upload

--- Iso/sources/$OEM$/$$/Setup/Scripts/Bin/GSecurity.bat
+++ Iso/sources/$OEM$/$$/Setup/Scripts/Bin/GSecurity.bat
@@ -5,17 +5,14 @@ takeown /f %windir%\System32\Oobe\useroobe.dll /A
 icacls %windir%\System32\Oobe\useroobe.dll /reset
 icacls %windir%\System32\Oobe\useroobe.dll /inheritance:r
 icacls "%systemdrive%\Users" /remove "Everyone"
-takeown /f "%USERPROFILE%\Desktop" /r /d y
+takeown /f "%USERPROFILE%\Desktop" /A /R /D y
+icacls "%USERPROFILE%\Desktop" /reset
 icacls "%USERPROFILE%\Desktop" /inheritance:r
-icacls "%USERPROFILE%\Desktop" /grant:r %username%:(OI)(CI)F /t /l /q /c
-icacls "%USERPROFILE%\Desktop" /remove "System" /t /c /l
-icacls "%USERPROFILE%\Desktop" /remove "Administrators" /t /c /l
-icacls "C:\Users\Public" /reset /T
-takeown /f "C:\Users\Public\Desktop" /r /d y
+icacls "%USERPROFILE%\Desktop" /grant:r "*S-1-2-1:F" /t /l /q /c
+takeown /f "C:\Users\Public\Desktop" /A /R /D y
+icacls "C:\Users\Public\Desktop" /reset
 icacls "C:\Users\Public\Desktop" /inheritance:r
-icacls "C:\Users\Public\Desktop" /grant:r %username%:(OI)(CI)F /t /l /q /c
-icacls "C:\Users\Public\Desktop" /remove "System" /t /c /l
-icacls "C:\Users\Public\Desktop" /remove "Administrators" /t /c /l
+icacls "C:\Users\Public\Desktop" /grant:r "*S-1-2-1:F" /t /l /q /c
 takeown /f "C:\Windows\System32\wbem" /A
 icacls "C:\Windows\System32\wbem" /reset
 icacls "C:\Windows\System32\wbem" /inheritance:r
