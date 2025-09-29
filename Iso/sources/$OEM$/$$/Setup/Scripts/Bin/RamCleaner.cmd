d6da7dc0f0916d6c2ebec53695e76c1986e19b95
Goran Štambuk <goran.stambuk.zadar@gmail.com>
2025-09-29 09:16:52 +0200

    Delete Iso/sources/$OEM$/$$/Setup/Scripts/Bin directory

--- Iso/sources/$OEM$/$$/Setup/Scripts/Bin/RamCleaner.cmd
+++
@@ -1,6 +0,0 @@
-:: Install RamCleaner
-mkdir %windir%\Setup\Scripts
-mkdir %windir%\Setup\Scripts\Bin
-copy /y emptystandbylist.exe %windir%\Setup\Scripts\Bin\emptystandbylist.exe
-copy /y RamCleaner.bat %windir%\Setup\Scripts\Bin\RamCleaner.bat
-schtasks /create /tn "RamCleaner" /xml "RamCleaner.xml" /ru "SYSTEM"
